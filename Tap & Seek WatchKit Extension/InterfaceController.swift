e2a9480d5f6b3e593cc9dea27433d38a9097efc0
Cameron Little <cameron@camlittle.com>
2015-04-14 19:03:16 -0700

    Watch app is working

--- Tap & Seek WatchKit Extension/InterfaceController.swift
+++ Tap & Seek WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,13 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
     @IBOutlet weak var table: WKInterfaceTable!
+    @IBOutlet weak var errorGroup: WKInterfaceGroup!
+    @IBOutlet weak var errorText: WKInterfaceLabel!
+
+    override init() {
+        super.init()
+        errorGroup.setHidden(true)
+    }
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -22,46 +29,53 @@ class InterfaceController: WKInterfaceController {
         WKInterfaceController.openParentApplication(request, reply: { (response: [NSObject : AnyObject]!, error: NSError!) -> Void in
             if error != nil {
                 println(error.usefulDescription)
+                self.errorGroup.setHidden(false)
+                self.errorText.setText(error.usefulDescription)
             } else {
                 let data = response["data"] as! [LocationInformation]
                 self.table.setNumberOfRows(data.count, withRowType: "LocationWKRow")
-                for i in 0...(data.count - 1) {
-                    let row = self.table.rowControllerAtIndex(i) as! RowController
-                    let loc = data[i]
-                    row.setText(loc.name)
-                    // this really should all be happening on the phone
-                    if let image = UIImage(data: loc.image) {
-                        // crop image to square
-                        let size: CGFloat = CGFloat(min(image.size.width, image.size.height))
-                        let x = (image.size.width - size) / 2.0
-                        let y = (image.size.height - size) / 2.0
+                if data.count > 0 {
+                    for i in 0...(data.count - 1) {
+                        let row = self.table.rowControllerAtIndex(i) as! RowController
+                        let loc = data[i]
+                        row.setText(loc.name)
+                        // this really should all be happening on the phone
+                        if let image = UIImage(data: loc.image) {
+                            // crop image to square
+                            let size: CGFloat = CGFloat(min(image.size.width, image.size.height))
+                            let x = (image.size.width - size) / 2.0
+                            let y = (image.size.height - size) / 2.0
 
-                        var cropRect: CGRect
-                        // respect image orientation metadata
-                        if (image.imageOrientation == .Left || image.imageOrientation == .Right) {
-                            cropRect = CGRectMake(y, x, size, size)
-                        } else {
-                            cropRect = CGRectMake(x, y, size, size)
+                            var cropRect: CGRect
+                            // respect image orientation metadata
+                            if (image.imageOrientation == .Left || image.imageOrientation == .Right) {
+                                cropRect = CGRectMake(y, x, size, size)
+                            } else {
+                                cropRect = CGRectMake(x, y, size, size)
+                            }
+
+                            let imageRef = CGImageCreateWithImageInRect(image.CGImage, cropRect)
+
+                            let square = UIImage(CGImage: imageRef)!
+                            /*
+                            // Figure out what our orientation is, and use that to form the rectangle
+                            var newSize = CGSizeMake(128, 128)
+
+                            // This is the rect that we've calculated out and this is what is actually used below
+                            let rect = CGRectMake(0, 0, newSize.width, newSize.height)
+
+                            // Actually do the resizing to the rect using the ImageContext stuff
+                            UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+                            square.drawInRect(rect)
+                            let resized = UIGraphicsGetImageFromCurrentImageContext()
+                            UIGraphicsEndImageContext()
+                            */
+                            row.setImage(square)
                         }
-
-                        let imageRef = CGImageCreateWithImageInRect(image.CGImage, cropRect)
-
-                        let square = UIImage(CGImage: imageRef)!
-                        /*
-                        // Figure out what our orientation is, and use that to form the rectangle
-                        var newSize = CGSizeMake(128, 128)
-
-                        // This is the rect that we've calculated out and this is what is actually used below
-                        let rect = CGRectMake(0, 0, newSize.width, newSize.height)
-
-                        // Actually do the resizing to the rect using the ImageContext stuff
-                        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-                        square.drawInRect(rect)
-                        let resized = UIGraphicsGetImageFromCurrentImageContext()
-                        UIGraphicsEndImageContext()
-                        */
-                        row.setImage(square)
                     }
+                } else {
+                    self.errorGroup.setHidden(false)
+                    self.errorText.setText("No Locations")
                 }
             }
         })
