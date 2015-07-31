bb1ad944dc0a5e6b5ab5440897b08c3871987bd6
Cameron Little <cameron@camlittle.com>
2015-07-31 13:58:46 -0700

    Create Private.example.swift

---
+++ SeeThere/Private.example.swift
@@ -0,0 +1,11 @@
+//
+//  Private.swift
+//  TapAndSeek
+//
+//  Created by Cameron Little on 2/2/15.
+//  Copyright (c) 2015 Cameron Little. All rights reserved.
+//
+
+import Foundation
+
+let googleAPIKey = "{{your api key here}}"
